38b4b7cb4c09e399678a8a025636e330be5d2975
silencio260 <faruq5840@gmail.com>
2024-08-16 11:24:02 +0100

    Implement ProductList Component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
-import Slider from "@/components/Slider"
+import ProductList from "@/components/ProductList";
+import Slider from "@/components/Slider";
 
 const HomePage = () => {
-  return (
-    <div className=''>
-      <Slider />
-    </div>
-  )
-}
+	return (
+		<div className=''>
+			<Slider />
 
-export default HomePage
+			<div className='mt-24 px-4 md:px-8 lg:px-16 xl:32 2xl:px-64'>
+				<h1 className='text-2xl'>Featured Product</h1>
+				<ProductList />
+			</div>
+		</div>
+	);
+};
+
+export default HomePage;
