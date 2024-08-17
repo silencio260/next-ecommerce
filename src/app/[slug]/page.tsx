e55e523fcf87e608683396ec137b6465252bd338
silencio260 <faruq5840@gmail.com>
2024-08-17 13:30:14 +0100

    Implement product page

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import CustomizeProducts from "@/components/CustomizeProducts";
+import ProductImages from "@/components/ProductImages";
+import React from "react";
 
 const SinglePage = () => {
-  return (
-    <div>SinglePage</div>
-  )
-}
+	return (
+		<div className='px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative flex flex-col lg:flex-row gap-16'>
+			{/* IMG */}
+			<div className='w-full lg:w-1/2 lg:sticky top-20 h-max'>
+				<ProductImages />
+			</div>
+			<CustomizeProducts />
+		</div>
+	);
+};
 
-export default SinglePage
+export default SinglePage;
