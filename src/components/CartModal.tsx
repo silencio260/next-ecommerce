cedd9e734b9e9f48538c1f3e188cdb02386629cb
silencio260 <faruq5840@gmail.com>
2024-08-21 19:32:11 +0100

    Implement statemanagement

--- src/components/CartModal.tsx
+++ src/components/CartModal.tsx
@@ -1,67 +1,128 @@
-'use client'
-import Image from 'next/image'
-import React from 'react'
+"use client";
+import Image from "next/image";
+import { useCartStore } from "@/hooks/useCartStore";
+import { media as wixMedia } from "@wix/sdk";
+import { useWixClient } from "@/hooks/useWixClient";
+import { currentCart } from "@wix/ecom";
 
-const CartModal = () => {
+const CartModal = async () => {
+	const wixClient = useWixClient();
+	const { cart, isLoading, removeItem } = useCartStore();
 
-    const cartItems = true
+	const handleCheckout = async () => {
+		try {
+			const checkout =
+				await wixClient.currentCart.createCheckoutFromCurrentCart({
+					channelType: currentCart.ChannelType.WEB,
+				});
 
-  return (
-    <div className="w-max absolute p-4 rounded-md shadow-[0_3px_10px_rgb(0,0,0,0.2)] bg-white top-12 right-0 flex flex-col gap-6 z-20">
-        {!cartItems ? (
-            <div>
-                Cart is Empty
-            </div>
-        ) : (
-            <>
-                <h2 className='text-xl'>Shopping Cart</h2>
-                <div className="flex flex-col gap-8">
-                    {/* ITEMS */}
-                    <div className='flex gap-4'>
-                        <Image 
-                            src={'/logo.png'}
-                            alt=''
-                            width={72}
-                            height={'96'}
-                            className='object-cover rounded-md'
-                        />
-                        <div className='flex flex-col justify-between w-full'>
-                            {/* TITLE */}
-                            <div className='flex items-center justify-between gap-8'>
-                                <h3 className='font-semibold'>Product Name</h3>
-                                <div className="p-1 bg-gray-50 rounded-sm">$49</div>
-                            </div>
-                            {/* DESCRIPTION */}
-                            <div className="text-sm text-gray-500">
-                                available
-                            </div>
-                            {/* BOTTM */}
-                            <div className='flex justify-between text-s,'>
-                                <span className='text-gray-500'>Qty. 2</span>
-                                <span className='text-blue-500'>Remove</span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                {/* BOTTOM */}
-                <div className="">
-                    <div className="flex items-center justify-between font-semibold">
-                        <span className="">Subtotal</span>
-                        <span className=''>$49</span>
-                        
-                    </div>
-                    <p className='text-gray-500 text-sm mt-2 mb-4'>
-                        Shipping and taxes calculated at checkout.
-                    </p>
-                    <div className="flex justify-between text-sm">
-                        <button className='rounded-md py-4 px-4 ring-1 ring-gray-300'>View Cart</button>
-                        <button className='rounded-md py-3 px-4 bg-black text-white'>Checkout</button>
-                    </div>
-                </div>
-            </>
-        )}
-    </div>
-  )
-}
+			const { redirectSession } =
+				await wixClient.redirects.createRedirectSession({
+					ecomCheckout: { checkoutId: checkout.checkoutId },
+					callbacks: {
+						postFlowUrl: window.location.origin,
+						thankYouPageUrl: `${window.location.origin}/success`,
+					},
+				});
 
-export default CartModal
+			if (redirectSession?.fullUrl) {
+				window.location.href = redirectSession.fullUrl;
+			}
+		} catch (err) {
+			console.log(err);
+		}
+	};
+
+	return (
+		<div className='w-max absolute p-4 rounded-md shadow-[0_3px_10px_rgb(0,0,0,0.2)] bg-white top-12 right-0 flex flex-col gap-6 z-20'>
+			{!cart.lineItems ? (
+				<div>Cart is Empty</div>
+			) : (
+				<>
+					<h2 className='text-xl'>Shopping Cart</h2>
+					<div className='flex flex-col gap-8'>
+						{/* ITEMS */}
+						<div className='flex flex-col gap-8'>
+							{/* ITEM */}
+							{cart.lineItems.map((item) => (
+								<div className='flex gap-4' key={item._id}>
+									{item.image && (
+										<Image
+											src={wixMedia.getScaledToFillImageUrl(
+												item.image,
+												72,
+												96,
+												{}
+											)}
+											alt=''
+											width={72}
+											height={96}
+											className='object-cover rounded-md'
+										/>
+									)}
+									<div className='flex flex-col justify-between w-full'>
+										{/* TOP */}
+										<div className=''>
+											{/* TITLE */}
+											<div className='flex items-center justify-between gap-8'>
+												<h3 className='font-semibold'>
+													{item.productName?.original}
+												</h3>
+												<div className='p-1 bg-gray-50 rounded-sm flex items-center gap-2'>
+													{item.quantity && item.quantity > 1 && (
+														<div className='text-xs text-green-500'>
+															{item.quantity} x{" "}
+														</div>
+													)}
+													${item.price?.amount}
+												</div>
+											</div>
+											{/* DESC */}
+											<div className='text-sm text-gray-500'>
+												{item.availability?.status}
+											</div>
+										</div>
+										{/* BOTTOM */}
+										<div className='flex justify-between text-sm'>
+											<span className='text-gray-500'>
+												Qty. {item.quantity}
+											</span>
+											<span
+												className='text-blue-500'
+												style={{
+													cursor: isLoading ? "not-allowed" : "pointer",
+												}}
+												onClick={() => removeItem(wixClient, item._id!)}>
+												Remove
+											</span>
+										</div>
+									</div>
+								</div>
+							))}
+						</div>
+					</div>
+					{/* BOTTOM */}
+					<div className=''>
+						<div className='flex items-center justify-between font-semibold'>
+							<span className=''>Subtotal</span>
+							<span className=''>$49</span>
+						</div>
+						<p className='text-gray-500 text-sm mt-2 mb-4'>
+							Shipping and taxes calculated at checkout.
+						</p>
+						<div className='flex justify-between text-sm'>
+							<button className='rounded-md py-4 px-4 ring-1 ring-gray-300'>
+								View Cart
+							</button>
+							<button className='rounded-md py-3 px-4 bg-black text-white'>
+								Checkout
+							</button>
+						</div>
+					</div>
+				</>
+			)}
+		</div>
+	);
+};
+
+export default CartModal;
--- src/components/NavIcons.tsx
+++ src/components/NavIcons.tsx
@@ -1,67 +1,81 @@
-'use client'
-import Image from 'next/image'
-import Link from 'next/link'
-import { useRouter } from 'next/navigation'
-import React, { useState } from 'react'
-import CartModal from './CartModal'
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
+import React, { useState } from "react";
+import CartModal from "./CartModal";
+import Cookies from "js-cookie";
+import { useWixClient } from "@/hooks/useWixClient";
 
 const NavIcons = () => {
+	const pathName = usePathname();
 
-    const [isProfileOpen, setIsProfileOpen] = useState(false)
-    const [isCartOpen, setIsCartOpen] = useState(false)
+	const wixClient = useWixClient();
+	const isLoggedIn = wixClient.auth.loggedIn();
 
-    const router = useRouter()
-    /////////
-    const isLoggedIn = false
-    const handleProfile = () => {
-        if(!isLoggedIn){
-            router.push('login')
-        }
-        setIsProfileOpen((prev) => !prev)
-    }
+	const [isProfileOpen, setIsProfileOpen] = useState(false);
+	const [isCartOpen, setIsCartOpen] = useState(false);
+	const [isLoading, setIsLoading] = useState(false);
 
-  return (
-    <div  className='flex items-center gap-4 xl:gap-6 relative'>
-        <Image
-            src="/profile.png"
-            alt=""
-            width={22}
-            height={22}
-            className="cursor-pointer"
-            onClick={handleProfile}
-        />
-        {isProfileOpen && (
-            <div className='absolute p-4 rounded-t-md top-12 left-0 text-sm shadow-[0_3px_10px_rgb(0,0,0,0.2)] z-20'>
-                <Link href={'/'}>Profile</Link>
-                <div className="mt-2 cursor-pointer">Logout</div>
-            </div>
-        )}
-        <Image
-            src="/notification.png"
-            alt=""
-            width={22}
-            height={22}
-            className="cursor-pointer"
-        />
-        <div className="relative cursor-pointer">
-            <Image
-                src="/cart.png"
-                alt=""
-                width={22}
-                height={22}
-                className="cursor-pointer"
-                onClick={() => setIsCartOpen((prev) => !prev)}
-            />
-            <div className="absolute -top-4 -right-4 w-6 h-6 bg-lama rounded-full text-white text-sm flex items-center justify-center">
-                2
-            </div>
-        </div>        
-        {isCartOpen && (
-            <CartModal />
-        )}
+	const router = useRouter();
+	/////////
+	const handleProfile = () => {
+		if (!isLoggedIn) {
+			router.push("login");
+		}
+		setIsProfileOpen((prev) => !prev);
+	};
 
-    </div>
-  )
-}
+	const handleLogout = async () => {
+		setIsLoading(true);
+		Cookies.remove("refreshToken");
+		const { logoutUrl } = await wixClient.auth.logout(window.location.href);
+		setIsLoading(false);
+		setIsProfileOpen(false);
+		router.push(logoutUrl);
+	};
 
-export default NavIcons
+	return (
+		<div className='flex items-center gap-4 xl:gap-6 relative'>
+			<Image
+				src='/profile.png'
+				alt=''
+				width={22}
+				height={22}
+				className='cursor-pointer'
+				onClick={handleProfile}
+			/>
+			{isProfileOpen && (
+				<div className='absolute p-4 rounded-t-md top-12 left-0 text-sm shadow-[0_3px_10px_rgb(0,0,0,0.2)] z-20'>
+					<Link href={"/"}>Profile</Link>
+					<div className='mt-2 cursor-pointer' onClick={handleLogout}>
+						{isLoading ? "Logging out" : "Logout"}
+					</div>
+				</div>
+			)}
+			<Image
+				src='/notification.png'
+				alt=''
+				width={22}
+				height={22}
+				className='cursor-pointer'
+			/>
+			<div className='relative cursor-pointer'>
+				<Image
+					src='/cart.png'
+					alt=''
+					width={22}
+					height={22}
+					className='cursor-pointer'
+					onClick={() => setIsCartOpen((prev) => !prev)}
+				/>
+				<div className='absolute -top-4 -right-4 w-6 h-6 bg-lama rounded-full text-white text-sm flex items-center justify-center'>
+					2
+				</div>
+			</div>
+			{isCartOpen && <CartModal />}
+		</div>
+	);
+};
+
+export default NavIcons;
