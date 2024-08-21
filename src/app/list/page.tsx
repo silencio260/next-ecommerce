c72123f2807a98de8cedba99955a2a84103ad1cd
silencio260 <faruq5840@gmail.com>
2024-08-21 16:12:53 +0100

    Integrate components with wix server

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,9 +1,18 @@
 import Filter from "@/components/Filter";
 import ProductList from "@/components/ProductList";
+import { wixClientServer } from "@/lib/wixClientServer";
 import Image from "next/image";
 import React from "react";
+import { WixClient } from "../../context/wixContext";
+import { collections } from "@wix/stores";
+import { Suspense } from "react";
 
-const ListPage = () => {
+const ListPage = async ({ searchParams }: { searchParams: any }) => {
+	const wixClient = await wixClientServer();
+
+	const cat = await wixClient.collections.getCollectionBySlug(
+		searchParams.cat || "all-products"
+	);
 	return (
 		<div className='px-4 md:px-8 lg:px-16 xl:32 2xl:px-64 relative'>
 			{/* CAMPAIGN */}
@@ -24,8 +33,17 @@ const ListPage = () => {
 			{/* FILTER */}
 			<Filter />
 			{/* PRODUCTS */}
-			<h1 className='mt-12 text-xl font-semibold'>Shoes For You!</h1>
-			<ProductList />
+			<h1 className='mt-12 text-xl font-semibold'>
+				{cat?.collection?.name} For You!
+			</h1>
+			<Suspense fallback={"loading"}>
+				<ProductList
+					categoryId={
+						cat.collection?._id || "00000000-000000-000000-000000000001"
+					}
+					searchParams={searchParams}
+				/>
+			</Suspense>
 		</div>
 	);
 };
--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -10,17 +10,43 @@ const PRODUCT_PER_PAGE = 20;
 const ProductList = async ({
 	categoryId,
 	limit,
+	searchParams,
 }: {
 	categoryId: String;
-	limit: number;
+	limit?: number;
+	searchParams?: any;
 }) => {
 	const wixClient = await wixClientServer();
 
-	const res = await wixClient.products
+	const productQuery = await wixClient.products
 		.queryProducts()
+		.startsWith("name", searchParams?.name || "")
 		.eq("collectionIds", categoryId)
+		.hasSome(
+			"productType",
+			searchParams?.type ? [searchParams.type] : ["physical", "digital"]
+		)
+		.gt("priceData.price", searchParams?.min || 0)
+		.lt("priceData.price", searchParams?.max || 999999)
 		.limit(limit || PRODUCT_PER_PAGE)
-		.find();
+		.skip(
+			searchParams?.page
+				? parseInt(searchParams.page) * (limit || PRODUCT_PER_PAGE)
+				: 0
+		);
+
+	if (searchParams?.sort) {
+		const [sortType, sortBy] = searchParams.sort.split(" ");
+
+		if (sortType === "asc") {
+			productQuery.ascending(sortBy);
+		}
+		if (sortType === "desc") {
+			productQuery.descending(sortBy);
+		}
+	}
+
+	const res = await productQuery.find();
 
 	return (
 		<div className='mt-12 flex gap-x-8 gap-y-16 justify-between flex-wrap'>
